353f00375a8db8726f4e1501bd36d59def9487bc
Alasgar M <mahmudov.alasgar@gmail.com>
2023-11-13 22:42:42 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Steps
+
+#### Server
+
+Open server directory.
+
+- run "npm install" and "npm start"  (to start server)
+
+
+#### React-app
+
+Open react-app directory 
+- run "npm install" and "npm run dev"  (to start react app)
---
+++ react-app/src/Items.jsx
@@ -0,0 +1,29 @@
+import SingleItem from './SingleItem';
+
+import { useFetchTasks } from './reactQueryCustomHooks';
+
+const Items = ({ items }) => {
+  const { isLoading, data, error, isError } = useFetchTasks();
+
+  if (isLoading) {
+    return <p style={{ marginTop: '1rem' }}>Loading</p>;
+  }
+
+  // if (isError) {
+  //   return <p style={{ marginTop: '1rem' }}>There was an error...</p>;
+  // }
+
+  if (error) {
+    return <p style={{ marginTop: '1rem' }}>{error.response.data}</p>;
+  }
+
+  return (
+    <div className='items'>
+      {data.taskList.map((item) => {
+        console.log(item.id);
+        return <SingleItem key={item.id} item={item} />;
+      })}
+    </div>
+  );
+};
+export default Items;
